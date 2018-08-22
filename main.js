2daafd4141f79d07f078ae135f5d506e82f79bc2
Steve Randy Tantra <steve.randy@gmail.com>
2018-08-22 15:08:47 +0800

    Enable plugins

--- main.js
+++ main.js
@@ -1,12 +1,7 @@
 require("./global");
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
-
-const path = require('path')
-const url = require('url')
+let {app, BrowserWindow} = require("electron");
+let path = require('path')
+let url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -83,6 +78,3 @@ app.on('activate', function () {
     createWindow()
   }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
